c96adbbe6b57dd820fe330f1553a7dc96435d1ef
Aniket Sharma <ansharma@quark.com>
2024-08-17 00:28:38 +0530

    Removed Extras

--- src/Utils/Dashboard.ts
+++
@@ -1,9 +0,0 @@
-class DashboardUtil {
-    static async createDashboard(name:string,description:string,datasource:string):Promise<Dashboard>{
-        const req = await fetch('/api/workbook',{
-            method: 'POST',
-            body: JSON.stringify({name,description,datasource})
-        });
-        return await req.json();
-    }
-}
