e830896fff33621aeb992d8edd97a1f764887024
Aniket Sharma <ansharma@quark.com>
2024-08-17 00:34:11 +0530

    Resolved Linting Errors

--- src/services/DashboardService.ts
+++
@@ -1,9 +0,0 @@
-import { randomUUID } from "crypto"
-
-export class DashboardService {
-    createDashboard = (name:string, description:string, datasource:string):Dashboard => {
-        const dashboardId:string = randomUUID();
-        const layout:DashboardLayout = {rows:[]};
-        return {id:dashboardId,name,description,datasource,layout};
-    }
-}
